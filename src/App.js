84bb1351f8f7720e513496e71fd6613c1fd817ff
hong <sjsjsj717@naver.com>
2021-07-14 14:35:29 +0900

    react | 210714 practice updating

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import TOC from "./component/TOC";
-import Content from "./component/Content"
-import Subject from './component/Subject';
+// import TOC from "./component/TOC";
+// import Content from "./component/Content"
+// import Subject from './component/Subject';
 import { Component } from 'react';
-
+import HelloWorld from "./component/HelloWorld";
+import Counter from "./component/Counter";
+import Parent from './component/Parent';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,28 +23,29 @@ class App extends Component {
     }
 
   }
+
   render() {
-    console.log('App render');
-    let _title, _desc = null;
-    if (this.state.mode === 'welcome') {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-    } else if (this.state.mode === 'read') {
-      let i = 0;
-      while(i < this.state.contents.length) {
-        let data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-    }
-    console.log('render', this);
+    // console.log('App render');
+    // let _title, _desc = null;
+    // if (this.state.mode === 'welcome') {
+    //   _title = this.state.welcome.title;
+    //   _desc = this.state.welcome.desc;
+    // } else if (this.state.mode === 'read') {
+    //   let i = 0;
+    //   while(i < this.state.contents.length) {
+    //     let data = this.state.contents[i];
+    //     if (data.id === this.state.selected_content_id) {
+    //       _title = data.title;
+    //       _desc = data.desc;
+    //       break;
+    //     }
+    //     i = i + 1;
+    //   }
+    // }
+    // console.log('render', this);
     return (
       <div className="App">
-          <Subject
+          {/* <Subject
             title={this.state.subject.title} 
             sub={this.state.subject.sub}
             // 이벤트에 함수를 설치
@@ -59,7 +62,10 @@ class App extends Component {
           }.bind(this)}
           data = {this.state.contents}
           ></TOC>
-          <Content title={_title} desc={_desc}></Content>
+          <Content title={_title} desc={_desc}></Content> */}
+          <HelloWorld title="helloworld" value="1" name="hong"></HelloWorld>
+          <Counter></Counter>
+          <Parent></Parent>
       </div>
     );
   }
